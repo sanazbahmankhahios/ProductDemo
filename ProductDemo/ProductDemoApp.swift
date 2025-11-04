7b9c58dff076b42aa932e1f4e892eb4437973332
Sanaz Bahmankhah <sanaz.bahmankhah@gmail.com>
2025-11-04 16:12:53 +0000

    Initial Commit

---
+++ ProductDemo/ProductDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductDemoApp.swift
+//  ProductDemo
+//
+//  Created by sanaz on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
