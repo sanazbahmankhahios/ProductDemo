1d0a43855fa3770466c89824c7f94fdad39615f8
Sanaz Bahmankhah <sanaz.bahmankhah@gmail.com>
2025-11-07 16:28:33 +0000

    Fix SPM Dependencies

--- Libraries/ProductListModule/Package.swift
+++ Libraries/ProductListModule/Package.swift
@@ -25,7 +25,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ProductListModule"),
+            name: "ProductListModule",
+            dependencies: ["ProductKit", "Kingfisher"]),
+        
 
     ]
 )
